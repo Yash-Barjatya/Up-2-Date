24a42f392892617ed224e90ef07e8873b5b1342c
Yash Barjatya <yashbarjatya11@gmail.com>
2023-07-30 11:20:54 +0530

    fixed mongoDB timeout bug

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose');
-const monogoURI = process.env.DATABASE_URI
-// console.log(monogoURI);
-const connectToMongo = () => {
-    mongoose.connect(monogoURI, { useNewUrlParser: true })
-        .then(() => {
-            console.log("Connected to MongoDB successfully..")
-        }).catch(err => {
-            console.log(err.message);
-        })
-    // mongoose.connect(monogoURI, () => {
-    //     console.log("connected to MongoDB successfully");
-    // })
-}
-module.exports = connectToMongo
-// mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true })
-//     .then(() => {
-//         console.log("Connected to MongoDB successfully..")
-//     }).catch(err => {
-//         console.log(err.message);
-//     })
+const mongoURI = process.env.DATABASE_URI;
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully.");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err.message);
+    }
+};
+
+module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,33 @@
 require('dotenv').config()
 const connectToMongo = require('./db');
-connectToMongo();
-const express = require('express')
-const port = process.env.PORT || 5000
-const app = express();
-const cors = require('cors')
+// connectToMongo();
+const startApp = async () => {
+    try {
+        await connectToMongo();
+        const express = require('express')
+        const port = process.env.PORT || 5000
+        const app = express();
+        const cors = require('cors')
 
-//middleware
-app.use(cors())// to fix the error of using 3000 in 5000 port
-app.use(express.json())
+        //middleware
+        app.use(cors())// to fix the error of using 3000 in 5000 port
+        app.use(express.json())
 
-// Routes
-app.get("/", (req, res) => {
-    res.send("Welcome to Up-2-Date server");
-});
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+        // Routes
+        app.get("/", (req, res) => {
+            res.send("Welcome to Up-2-Date server");
+        });
+        app.use('/api/auth', require('./routes/auth'))
+        app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
-    console.log(`Up-2-Date server is listening to port ${port}`)
-})
+        app.listen(port, () => {
+            console.log(`Up-2-Date server is listening to port ${port}`)
+        })
+        // Do other tasks related to starting your application here.
+    } catch (err) {
+        console.error("Error starting the application:", err.message);
+        // Perform any necessary error handling.
+    }
+};
+
+startApp();
